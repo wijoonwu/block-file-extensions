57f8315848d5781a64d10fca24e6783fb5fae28f
wijoonwu <devjoon@nate.com>
2023-03-13 21:45:41 +0900

    Create FixedExtensionRepository.java

---
+++ src/main/java/com/flow/board/repository/FixedExtensionRepository.java
@@ -0,0 +1,8 @@
+package com.flow.board.repository;
+
+import com.flow.board.web.entity.FixedExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FixedExtensionRepository extends JpaRepository<FixedExtension, Long > {
+
+}
