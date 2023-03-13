21d87f0c56d6dd3f7643945c1e85a862d0647327
wijoonwu <devjoon@nate.com>
2023-03-13 21:45:51 +0900

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into FIXED_EXTENSION (id, name, use) values (1, 'bat', 0);
+insert into FIXED_EXTENSION (id, name, use) values (2, 'cmd', 0);
+insert into FIXED_EXTENSION (id, name, use) values (3, 'com', 0);
+insert into FIXED_EXTENSION (id, name, use) values (4, 'cpl', 0);
+insert into FIXED_EXTENSION (id, name, use) values (5, 'exe', 0);
+insert into FIXED_EXTENSION (id, name, use) values (6, 'scr', 0);
+insert into FIXED_EXTENSION (id, name, use) values (7, 'js', 0);
+
+insert into CUSTOM_EXTENSION (name) values ('exe'), ('sh'), ('ch');
