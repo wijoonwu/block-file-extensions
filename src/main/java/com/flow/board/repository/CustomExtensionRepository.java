7eb35490671d1290be95afbd24c7ea8f7fb7ed3f
wijoonwu <devjoon@nate.com>
2023-03-13 21:45:37 +0900

    Create CustomExtensionRepository.java

---
+++ src/main/java/com/flow/board/repository/CustomExtensionRepository.java
@@ -0,0 +1,8 @@
+package com.flow.board.repository;
+
+import com.flow.board.web.entity.CustomExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomExtensionRepository extends JpaRepository<CustomExtension, Long> {
+
+}
