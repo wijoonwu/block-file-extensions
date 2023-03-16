4a2eebe550a6f66417db7091dfea8cbb36241464
wijoonwu <devjoon@nate.com>
2023-03-16 17:17:15 +0900

    Refactor: return 메시지 상수 선언

--- src/main/java/com/flow/board/service/CustomExtensionService.java
+++ src/main/java/com/flow/board/service/CustomExtensionService.java
@@ -16,34 +16,39 @@ public class CustomExtensionService {
 
     private final CustomExtensionRepository customExtensionRepository;
 
+    protected static final String EXISTING_EXTENSION_MSG = "이미 추가된 확장자 입니다.";
+    protected static final String MAX_SIZE_EXCEEDED_MSG = "200개를 초과하여 등록할 수 없습니다.";
+    protected static final String MAX_LENGTH_EXCEEDED_MSG = "20글자를 초과할 수 없습니다.";
+    protected static final String ADD_SUCCESS_MSG = "추가 되었습니다.";
+    protected static final String DELETE_SUCCESS_MSG = "삭제되었습니다";
+
     @Transactional(readOnly = true)
     public List<CustomExtension> readAll() {
         return customExtensionRepository.findAll();
     }
 
     public String createCustomExtension(CustomExtensionDto customExtensionDto) {
-        int totalCustomExtensionSize = customExtensionRepository.findAll().size();
         String name = customExtensionDto.getName();
 
         if (customExtensionRepository.findByName(name).isPresent()) {
-            return "이미 추가된 확장자 입니다.";
+            return EXISTING_EXTENSION_MSG;
         }
-        if (totalCustomExtensionSize > CustomExtension.MAX_SIZE) {
-            return "200개를 초과하여 등록할 수 없습니다.";
+        if (customExtensionRepository.findAll().size() > CustomExtension.MAX_SIZE) {
+            return MAX_SIZE_EXCEEDED_MSG;
         }
 
         if (customExtensionDto.getName().length() > CustomExtension.MAX_LENGTH ) {
-            return "20글자를 초과할 수 없습니다.";
+            return MAX_LENGTH_EXCEEDED_MSG;
         }
 
         CustomExtension customExtension = customExtensionDto.toEntity();
         customExtensionRepository.save(customExtension);
-        return "추가 되었습니다.";
+        return ADD_SUCCESS_MSG;
     }
 
     public String deleteCustomExtension(CustomExtensionDto customExtensionDto) {
         String name = customExtensionDto.getName();
         customExtensionRepository.deleteByName(name);
-        return "삭제되었습니다";
+        return DELETE_SUCCESS_MSG;
     }
 }
--- src/main/java/com/flow/board/service/FixedExtensionService.java
+++ src/main/java/com/flow/board/service/FixedExtensionService.java
@@ -1,9 +1,9 @@
 package com.flow.board.service;
 
-
 import com.flow.board.repository.FixedExtensionRepository;
 import com.flow.board.web.dto.FixedExtensionDto;
 import com.flow.board.entity.FixedExtension;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,19 +14,24 @@ import java.util.List;
 @Transactional
 @Service
 public class FixedExtensionService {
-    private final FixedExtensionRepository fixedExtensionRepository;
+    protected final FixedExtensionRepository fixedExtensionRepository;
+
+    protected static final String EXTENSION_IN_USE = "사용상태로 변경되었습니다.";
+    protected static final String EXTENSION_NOT_IN_USE = "미사용 상태로 변경 되었습니다.";
+    protected static final String EXTENSION_NOT_FOUND = "확장자를 찾을 수 없습니다.";
 
     @Transactional(readOnly = true)
     public List<FixedExtension> readAll() {
-       return fixedExtensionRepository.findAll();
+        return fixedExtensionRepository.findAll();
     }
 
     public String updateFixedExtension(FixedExtensionDto fixedExtensionDto) {
         String name = fixedExtensionDto.getName();
-        FixedExtension fixedExtension = fixedExtensionRepository.findByName(name);
-        fixedExtension.updateUse();
-        if(fixedExtension.isUse()){
-            return "사용 상태로 변경되었습니다.";
-        } return "미사용 상태로 변경되었습니다.";
+        Optional<FixedExtension> fixedExtension = fixedExtensionRepository.findByName(name);
+
+        return fixedExtension.map(extension -> {
+            extension.updateUse();
+            return extension.isUse() ? EXTENSION_IN_USE : EXTENSION_NOT_IN_USE;
+        }).orElse(EXTENSION_NOT_FOUND);
     }
 }
